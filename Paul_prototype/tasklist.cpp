4ff555e05a05263b70e3a038fe7a3f8fbb05eafa
DinoL <grech.leonid@gmail.com>
2016-09-18 19:30:24 +0300

    (*) Use task id in the list

--- Paul_prototype/tasklist.cpp
+++ Paul_prototype/tasklist.cpp
@@ -3,19 +3,25 @@
 namespace Model
 {
 
-    TaskList::TaskList()
+    void TaskList::addTask(TaskId id, const Task& task)
     {
-
+        tasks_.emplace(id, std::make_unique<Task>(task));
     }
 
-    void TaskList::addTask(const Task &task)
+    void TaskList::addTask(TaskId id, const TaskSettings& settings)
     {
-        tasks_.push_back(task);
+        tasks_.emplace(id, std::make_unique<Task>(settings));
     }
 
     size_t TaskList::tasksCount() const
     {
         return tasks_.size();
+    }
+
+    Task* TaskList::findTask(TaskId id) const
+    {
+        auto& it = tasks_.find(id);
+        return (it != tasks_.end()) ? it->second.get() : nullptr;
     }
 
 }
--- Paul_prototype/tasklist.h
+++ Paul_prototype/tasklist.h
@@ -1,22 +1,35 @@
 #pragma once
 
-#include <vector>
+#include <map>
 #include "task.h"
 
 namespace Model
 {
 
+    /// Represents a list of tasks
     class TaskList
     {
     public:
-        TaskList();
+        /// Add task to the list
+        /// \param  task  task to add
+        void addTask(TaskId id, const Task& task);
 
-        void addTask(const Task& task);
+        /// Add task to the list
+        /// \param  settings settings to be used for the task
+        void addTask(TaskId id, const TaskSettings& settings);
 
+        /// Get tasks count in the list
+        /// \return  tasks count
         size_t tasksCount() const;
 
+        /// Find task by id in the list
+        /// \param  id  task id to search
+        /// \return task with the desired id or nullptr if not found
+        Task* findTask(TaskId id) const;
+
     private:
-        std::vector<Task> tasks_;
+
+        std::map<TaskId, TaskPtr> tasks_; /// tasks in the list
     };
 
 }
