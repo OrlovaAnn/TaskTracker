ce7e328278da5da37015c5a0efa952a7a8bbd53c
DinoL <grech.leonid@gmail.com>
2016-09-26 00:27:34 +0300

    (+) Allow to modify task type from the settings

--- Paul_prototype/createtaskdialog.cpp
+++ Paul_prototype/createtaskdialog.cpp
@@ -2,6 +2,23 @@
 
 #include "ui_createtaskdialog.h"
 
+static const std::map<int, Model::TaskStateType> s_groupBoxItems =
+{
+    {0, Model::TaskStateType::Open},
+    {1, Model::TaskStateType::InProgress},
+    {2, Model::TaskStateType::Closed}
+};
+
+int getIndex(const Model::TaskStateType& state)
+{
+    for(auto it : s_groupBoxItems)
+    {
+        if(it.second == state)
+            return it.first;
+    }
+    return -1;
+}
+
 CreateTaskDialog::CreateTaskDialog()
     : ui_(std::make_unique<Ui::CreateTaskDialog>())
 {
@@ -16,11 +33,15 @@ CreateTaskDialog::CreateTaskDialog(const Model::TaskSettings& settings)
     ui_->horizontalSlider->setMaximum(100);
     ui_->lineEdit->setText(settings.name_);
     ui_->textEdit->setText(settings.description_);
+    ui_->statusComboBox->setCurrentIndex(getIndex(settings.state().type_));
 }
 
 Model::TaskSettings CreateTaskDialog::getSettings() const
 {
-    return Model::TaskSettings{ui_->lineEdit->text(),
-                               ui_->textEdit->toPlainText(),
-                               static_cast<double>(ui_->horizontalSlider->value())/ui_->horizontalSlider->maximum()};
+    Model::TaskSettings settings{ui_->lineEdit->text(),
+                ui_->textEdit->toPlainText(),
+                static_cast<double>(ui_->horizontalSlider->value())/ui_->horizontalSlider->maximum()};
+    const int currentIndex = ui_->statusComboBox->currentIndex();
+    settings.state().type_ = s_groupBoxItems.at(currentIndex);
+    return settings;
 }
