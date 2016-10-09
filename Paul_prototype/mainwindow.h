762ff492b42af80528cccc2c9aa690eda739729b
DinoL <grech.leonid@gmail.com>
2016-10-09 20:39:33 +0300

    (+) Add some comments, rename slot

--- Paul_prototype/mainwindow.h
+++ Paul_prototype/mainwindow.h
@@ -27,25 +27,36 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    /// Called a user wants to add new task
+    void on_addNewTaskButton_clicked();
 
     void on_filterOpen_toggled(bool);
     void on_filterInProgress_toggled(bool);
     void on_filterClosed_toggled(bool);
+    /// Called when task changes its state
+    /// \param  id of the task which changed
+    void on_task_changed(Model::TaskId id);
 
-    void on_task_type_changed(Model::TaskId id);
 
 private:
     std::unique_ptr<Ui::MainWindow> ui_;
 
+    // Stores created widgets corresponding to some task id
     using WidgetPtr = std::unique_ptr<TasklistItemWidget>;
     std::map<Model::TaskId, WidgetPtr> tasks_;
 
+
+    /// Create and add new task to the list
+    /// \param  settings to be used
     void drawNewTask(const Model::TaskSettings& settings);
 
+    /// Update visibility of all tasks or single task
     void updateTasksVisibility();
     void updateTaskVisibility(Model::TaskId id);
     void updateTaskVisibility(TasklistItemWidget& task);
+
+    /// Add some default tasks to the list
+    /// (for the testing needs only)
     void addDefaultTasks();
     bool isFilterChecked(Model::TaskStateType type) const;
 };
